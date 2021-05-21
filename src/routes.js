6b267741d15a95c7ca0912e5828004a08a70b00c
RenatoRossetoOutSmart <renato.rosseto@outsmartdigital.com.br>
2021-05-21 12:06:21 -0300

    chore: creating not found route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home'
+import NoMatch from './pages/404'
 
 export default function Routes() {
     return (
       <BrowserRouter>
           <Switch>
               <Route path="/" exact component={Home} />
+              <Route component={NoMatch} />
           </Switch>
       </BrowserRouter>
     )
